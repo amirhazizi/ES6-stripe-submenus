4ef8280fb28374310ebcdc651cce47d1583a2664
amir <amirazizihosein@gmail.com>
2023-02-17 23:56:30 +0330

    update sidebar

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ toggleBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebarWrapper.classList.remove("show")
 })
+sidebar.innerHTML = sublinks
+  .map((item) => {
+    const { links, page } = item
+    return `<article>
+  <h4>${page}</h4>
+  <div class="sidebar-sublinks">
+  ${links
+    .map((link) => {
+      return `<a href="${link.url}">
+        <i class='${link.icon}'></i>${link.label}
+        </a>`
+    })
+    .join("")}
+    </div>
+    </article>`
+  })
+  .join("")
