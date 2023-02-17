0f29c8bc7618d0899a05f0b3595fcbf784db33ce
amir <amirazizihosein@gmail.com>
2023-02-17 23:40:42 +0330

    update app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+import sublinks from "./data.js"
+
+const toggleBtn = document.querySelector(".toggle-btn")
+const closeBtn = document.querySelector(".close-btn")
+const sidebarWrapper = document.querySelector(".sidebar-wrapper")
+const sidebar = document.querySelector(".sidebar-links")
+const linkBtn = [...document.querySelectorAll(".link-btn")]
+const hero = document.querySelector(".hero")
+const nav = document.querySelector(".nav")
+
+// hide/show sidebar
+toggleBtn.addEventListener("click", () => {
+  sidebarWrapper.classList.add("show")
+})
+closeBtn.addEventListener("click", () => {
+  sidebarWrapper.classList.remove("show")
+})
